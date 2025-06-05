43f322aa154e39760bfe78a095d777c42b0316d9
Martin Hanicinec <hanicinecm@gmail.com>
2025-06-05 21:07:29 +0200

    chore: progress the setup script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,15 @@ set -u  # Treat unset variables as errors
 set -o pipefail  # Better error propagation in pipelines
 
 
-echo -e "\n━━━━━━━━━━━━━━━━━━━━━━ 🔧 Installing zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
+echo -e "\n━━━━━━━━━━━━━━━━ 🔧 Updating system packages ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
+
+sudo apt update
+sudo apt upgrade -y
+
+
+echo -e "\n━━━━━━━━━━━━━━━━ 🔧 Installing zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
 
 if ! command -v zsh &> /dev/null; then
-    sudo apt update
     sudo apt install -y zsh
     echo "✅ zsh installed."
 else
@@ -16,9 +21,10 @@ else
 fi
 
 
-echo -e "\n━━━━━━━━━━━━━━━━━━━━━━ 🔧 Installing oh-my-zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
+echo -e "\n━━━━━━━━━━━━━━━━ 🔧 Installing oh-my-zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
 
 OMZ_URL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+OMZ_DIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZ_DIR" ]; then
     RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL "$OMZ_URL")"
     echo "✅ oh-my-zsh installed."
