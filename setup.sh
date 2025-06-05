74062acd1ffb755ff56c053d627ded13ad5168f7
Martin Hanicinec <hanicinecm@gmail.com>
2025-06-05 21:03:07 +0200

    chore: progress the setup script

--- setup.sh
+++ setup.sh
@@ -5,9 +5,8 @@ set -u  # Treat unset variables as errors
 set -o pipefail  # Better error propagation in pipelines
 
 
-echo "🔧 Installing zsh..."
+echo -e "\n━━━━━━━━━━━━━━━━━━━━━━ 🔧 Installing zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
 
-# Check if zsh is already installed
 if ! command -v zsh &> /dev/null; then
     sudo apt update
     sudo apt install -y zsh
@@ -16,11 +15,20 @@ else
     echo "ℹ️ zsh is already installed."
 fi
 
-# Set zsh as default shell for current user
-if [ "$SHELL" != "$(which zsh)" ]; then
-    echo "🔄 Changing default shell to zsh..."
-    chsh -s "$(which zsh)"
-    echo "✅ Default shell changed to zsh. You may need to log out and back in."
+
+echo -e "\n━━━━━━━━━━━━━━━━━━━━━━ 🔧 Installing oh-my-zsh  ━━━━━━━━━━━━━━━━━━━━━━━━━━\n"
+
+OMZ_URL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+if [ ! -d "$OMZ_DIR" ]; then
+    RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL "$OMZ_URL")"
+    echo "✅ oh-my-zsh installed."
 else
-    echo "ℹ️ zsh is already your default shell."
+    echo "ℹ️ oh-my-zsh is already installed."
 fi
+
+
+# # Set zsh as default shell for current user
+# if [ "$SHELL" != "$(which zsh)" ]; then
+#     chsh -s "$(which zsh)"
+#     echo "✅ Default shell changed to zsh. You may need to log out and back in."
+# fi
