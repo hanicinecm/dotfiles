18b3259da5bcb6344cbc58ae9a85bdb6e20841ab
Martin Hanicinec <hanicinecm@gmail.com>
2025-06-13 01:54:06 +0200

    ci: try fixing a bug in the ci

--- scripts/01-packages.sh
+++ scripts/01-packages.sh
@@ -17,7 +17,7 @@ sudo apt install -y "${packages[@]}"
 if ! command -v uv &> /dev/null; then
     echo "🔧 Installing uv package manager..."
     curl -LsSf https://astral.sh/uv/install.sh | sh
-    source "$HOME/.local/bin/env"  # To ensure that uv is available for setup.sh script
+    export PATH="$HOME/.local/bin:$PATH" # To ensure that uv is available for setup.sh
 else
     echo "ℹ️ uv is already installed."
 fi
